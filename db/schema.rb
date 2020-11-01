5d4f5cc751bde77a7216c95a1edf7d67bdb01fe4
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-11-01 16:09:00 -0400

    Config postgis database adapter

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_002123) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
 
-  create_table "ratings", force: :cascade do |t|
-    t.bigint "store_id", null: false
-    t.integer "value"
-    t.string "opinion"
-    t.string "user_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["store_id"], name: "index_ratings_on_store_id"
-  end
-
-  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
-    t.string "auth_name", limit: 256
-    t.integer "auth_srid"
-    t.string "srtext", limit: 2048
-    t.string "proj4text", limit: 2048
-  end
-
-# Could not dump table "stores" because of following StandardError
-#   Unknown type 'geography(Point,4326)' for column 'lonlat'
-
-  add_foreign_key "ratings", "stores"
 end
